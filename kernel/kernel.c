35cfac2a7a172e2d6142221f6f867d8c45f5a3e7
nahK994 <nkskl6@gmail.com>
2025-12-21 15:42:02 +0600

    make clear_screen and handle new line

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,65 +1,56 @@
 // kernel/kernel.c
 
+#define VIDEO_MEMORY 0xB8000
+#define WHITE_ON_BLACK 0x0F
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+
+int cursor_position = 0;
+
+// screen clear করে
+void clear_screen() {
+    char* video_memory = (char*) VIDEO_MEMORY;
+
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+        video_memory[i * 2] = ' ';
+        video_memory[i * 2 + 1] = WHITE_ON_BLACK;
+    }
+
+    // cursor reset
+    cursor_position = 0;
+}
+
+// newline handle করে
+void new_line() {
+    int current_line = cursor_position / SCREEN_WIDTH;
+    cursor_position = (current_line + 1) * SCREEN_WIDTH;
+}
+
+// একটা character print করে
+void print_char(char c) {
+    if (c == '\n') {
+        new_line();
+        return;
+    }
+
+    char* video_memory = (char*) VIDEO_MEMORY;
+    video_memory[cursor_position * 2] = c;
+    video_memory[cursor_position * 2 + 1] = WHITE_ON_BLACK;
+    cursor_position++;
+}
+
+// string print করে
+void print(const char* str) {
+    int i = 0;
+    while (str[i] != '\0') {
+        print_char(str[i]);
+        i++;
+    }
+}
+
+// kernel entry point
 void kernel_main() {
-    char* video_memory = (char*) 0xB8000;
-
-    video_memory[0] = 'M';
-    video_memory[1] = 0x0F;
-
-    video_memory[2] = 'i';
-    video_memory[3] = 0x0F;
-
-    video_memory[4] = 'n';
-    video_memory[5] = 0x0F;
-
-    video_memory[6] = 'i';
-    video_memory[7] = 0x0F;
-
-    video_memory[8] = 'O';
-    video_memory[9] = 0x0F;
-
-    video_memory[10] = 'S';
-    video_memory[11] = 0x0F;
-
-    video_memory[12] = ' ';
-    video_memory[13] = 0x0F;
-
-    video_memory[14] = 'i';
-    video_memory[15] = 0x0F;
-
-    video_memory[16] = 's';
-    video_memory[17] = 0x0F;
-
-    video_memory[18] = ' ';
-    video_memory[19] = 0x0F;
-
-    video_memory[20] = 'r';
-    video_memory[21] = 0x0F;
-
-    video_memory[22] = 'u';
-    video_memory[23] = 0x0F;
-
-    video_memory[24] = 'n';
-    video_memory[25] = 0x0F;
-
-    video_memory[26] = 'n';
-    video_memory[27] = 0x0F;
-
-    video_memory[28] = 'i';
-    video_memory[29] = 0x0F;
-
-    video_memory[30] = 'n';
-    video_memory[31] = 0x0F;
-
-    video_memory[32] = 'g';
-    video_memory[33] = 0x0F;
-
-    video_memory[34] = '.';
-    video_memory[35] = 0x0F;
-
-    video_memory[36] = '.';
-    video_memory[37] = 0x0F;
-
-    video_memory[38] = '.';
-    video_memory[39] = 0x0F;
+    clear_screen();
+    print("MiniOS is running...\n");
+    print("Welcome to your first OS!");
 }
