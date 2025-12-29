1ebef39976be396801b9741f53a091fc7febe14c
nahK994 <nkskl6@gmail.com>
2025-12-29 08:51:59 +0600

    add memory limit

--- kernel/memory.c
+++ kernel/memory.c
@@ -3,6 +3,9 @@
 extern char _kernel_end;
 
 static uint32_t free_memory_address;
+#define HEAP_SIZE (1024 * 1024)   // 1 MB
+static uint32_t heap_start;
+static uint32_t heap_end;
 
 #define ALIGNMENT 4
 
@@ -15,11 +18,20 @@ static uint32_t align_up(uint32_t addr) {
 
 
 void init_memory() {
-    free_memory_address = (uint32_t)&_kernel_end;
+    heap_start = align_up((uint32_t)&_kernel_end);
+    free_memory_address = heap_start;
+    heap_end = heap_start + HEAP_SIZE;
 }
 
 void* kmalloc(uint32_t size) {
     uint32_t aligned_address = align_up(free_memory_address);
-    free_memory_address = aligned_address + size;
+    uint32_t next_free = aligned_address + size;
+
+    if (next_free > heap_end) {
+        print("KERNEL PANIC: Out of memory!\n");
+        while (1) { }
+    }
+
+    free_memory_address = next_free;
     return (void*)aligned_address;
 }
